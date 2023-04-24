6662a49e6c522c75a8ee250f90a012d13e511694
TechnoStudy <ismettechno@gmail.com>
2023-04-24 18:52:57 +0100

    checkstateInResponseBody

--- src/test/java/ZippoTest.java
+++ src/test/java/ZippoTest.java
@@ -66,11 +66,28 @@ public class ZippoTest {
                // pm.response.json().id -> body.id
         ;
     }
+//
+//    PM                            RestAssured
+//    body.country                  body("country")
+//    body.'post code'              body("post code")
+//    body.places[0].'place name'   body("places[0].'place name'")
+//    body.places.'place name'      body("places.'place name'")
+//    bütün place nameleri bir arraylist olarak verir
+//    https://jsonpathfinder.com/
 
+    @Test
+    public void checkstateInResponseBody(){
+        given()
 
+                .when()
+                .get("http://api.zippopotam.us/us/90210")
 
-
-
+                .then()
+                //.log().body()
+                .statusCode(200)
+                .body("places[0].state", equalTo("California"))
+        ;
+    }
 
 
 
