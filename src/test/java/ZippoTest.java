f32ed394749f51c2eae6737840bfcd264fe810d1
TechnoStudy <ismettechno@gmail.com>
2023-04-24 18:21:57 +0100

    Initial commit

---
+++ src/test/java/ZippoTest.java
@@ -0,0 +1,64 @@
+import io.restassured.http.ContentType;
+import org.testng.annotations.Test;
+import static io.restassured.RestAssured.*;
+
+public class ZippoTest {
+
+    @Test
+    public void test(){
+
+        given()
+                // Hazırlık işlemleri : (token,send body, parametreler)
+
+                .when()
+                // endpoint (url), metodu
+
+                .then()
+                // assertion, test, data işlemleri
+        ;
+    }
+
+    @Test
+    public void statusCodeTest(){
+
+        given()
+
+                .when()
+                .get("http://api.zippopotam.us/us/90210")
+
+                .then()
+                .log().body()    // dönen body json datat sı,   log.all()
+                .statusCode(200) // dönüş kodu 200 mü
+        ;
+    }
+
+
+    @Test
+    public void contentTypeTest(){
+
+        given()
+
+                .when()
+                .get("http://api.zippopotam.us/us/90210")
+
+                .then()
+                .log().body()    // dönen body json datat sı,   log.all()
+                .statusCode(200) // dönüş kodu 200 mü
+                .contentType(ContentType.JSON) // dönen sonuç JSON mı
+        ;
+    }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+}
