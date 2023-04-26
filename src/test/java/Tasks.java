30374f32a75939fb92c0326f7c63bcc416238a01
TechnoStudy <ismettechno@gmail.com>
2023-04-26 17:34:42 +0100

    Task 3

--- src/test/java/Tasks.java
+++ src/test/java/Tasks.java
@@ -55,6 +55,31 @@ public class Tasks {
     }
 
 
+    /**
+     * Task 3
+     * create a request to https://jsonplaceholder.typicode.com/todos/2
+     * expect status 200
+     * expect content type JSON
+     * expect title in response body to be "quis ut nam facilis et officia qui"
+     */
+
+    @Test
+    public void task3()
+    {
+                given()
+                        .when()
+                        .get("https://jsonplaceholder.typicode.com/todos/2")
+
+                        .then()
+                        .log().body()
+                        .statusCode(200)
+                        .contentType(ContentType.JSON)
+                        .body("title", equalTo("quis ut nam facilis et officia qui"))
+                ;
+    }
+
+
+
 
 
 }
